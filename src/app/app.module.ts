b08bb203c015c2828d97dc3bed7678348c987808
[ElieZoetaba] <[zoetabaelie@gmail.com]>
2022-07-05 15:09:54 +0000

    Components link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { PersonnelComponent } from './personnel/personnel.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProfesorsComponent } from './profesors/profesors.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { IncriptionComponent } from './incription/incription.component';
+import { FooterComponent } from './footer/footer.component';
+import { AProposComponent } from './a-propos/a-propos.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    PersonnelComponent,
+    GalleryComponent,
+    ProfesorsComponent,
+    BlogComponent,
+    ContactComponent,
+    IncriptionComponent,
+    FooterComponent,
+    AProposComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
